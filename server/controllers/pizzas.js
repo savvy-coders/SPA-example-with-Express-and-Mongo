d8b185be81ffec8cbb2643f4e6db91463668e27e
Matt Thomas <matsinet@gmail.com>
2023-03-02 09:58:56 -0600

    Update error handling

--- server/controllers/pizzas.js
+++ server/controllers/pizzas.js
@@ -5,22 +5,29 @@ const Pizza = require("../models/pizza");
 
 router.post("/", (request, response) => {
   const newPizza = new Pizza.model(request.body);
-  newPizza.save((err, pizza) => {
-    return err ? response.sendStatus(500).json(err) : response.json(pizza);
+  newPizza.save((error, pizza) => {
+    if (error.name === 'ValidationError') return response.status(400).json(error.errors);
+    if (error) return response.status(500).json(error.errors);
+
+    response.json(pizza);
   });
 });
 
 router.get("/", (request, response) => {
   Pizza.model.find({}, (error, data) => {
-    if (error) return response.sendStatus(500).json(error);
-    return response.json(data);
+    if (error.name === 'ValidationError') return response.status(400).json(error.errors);
+    if (error) return response.status(500).json(error.errors);
+
+    response.json(data);
   });
 });
 
 router.get("/:id", (request, response) => {
   Pizza.model.findById(request.params.id, (error, data) => {
-    if (error) return response.sendStatus(500).json(error);
-    return response.json(data);
+    if (error.name === 'ValidationError') return response.status(400).json(error.errors);
+    if (error) return response.status(500).json(error.errors);
+
+    response.json(data);
   });
 });
 
@@ -37,16 +44,19 @@ router.put("/:id", (request, response) => {
       }
     },
     (error, data) => {
-      if (error) return response.sendStatus(500).json(error);
-      return response.json(request.body);
+      if (error.name === 'ValidationError') return response.status(400).json(error.errors);
+      if (error) return response.status(500).json(error.errors);
+
+      response.json(data);
     }
   );
 });
 
 router.delete("/:id", (request, response) => {
   Pizza.model.findByIdAndRemove(request.params.id, {}, (error, data) => {
-    if (error) return response.sendStatus(500).json(error);
-    return response.json(data);
+    if (error) return response.status(500).json(error.errors);
+
+    response.json(data);
   });
 });
 
