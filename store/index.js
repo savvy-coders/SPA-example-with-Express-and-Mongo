52b65efb466b769288e76dedd79750e56b952a0a
Matt Thomas <matsinet@gmail.com>
2023-03-14 18:11:25 -0500

    Added Viewnotfound to aggregator

--- store/index.js
+++ store/index.js
@@ -3,3 +3,4 @@ export { default as Bio } from "./Bio";
 export { default as Links } from "./Links";
 export { default as Pizza } from "./Pizza";
 export { default as Order } from "./Order";
+export { default as Viewnotfound } from "./Viewnotfound";
