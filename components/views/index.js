640502f236165bab825d51bad4662e3b52f5d86d
Matt Thomas <matsinet@gmail.com>
2023-06-08 12:26:48 -0500

    Move view out of components

--- components/views/index.js
+++
@@ -1,5 +0,0 @@
-export { default as home } from "./home";
-export { default as aboutMe } from "./aboutMe";
-export { default as pizza } from "./pizza";
-export { default as order } from "./order";
-export { default as viewNotFound } from "./viewNotFound";
