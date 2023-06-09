bfc9f9d71f54e7af550300d11f8c1c0fd3eb3867
Matt Thomas <matsinet@gmail.com>
2023-06-09 13:35:43 -0500

    Add error handling with notification component

--- store/links.js
+++
@@ -1,7 +0,0 @@
-export default [
-  { url: "/home", text: "Home" },
-  { url: "/about-me", text: "About Me" },
-  { url: "/order", text: "Order a Pizza" },
-  { url: "/pizza", text: "See all the Pizzas" },
-  { url: "https://www.google.com", text: "Google", external: true }
-];
