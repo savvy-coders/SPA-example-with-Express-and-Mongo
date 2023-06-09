c9a6fb0cee3251e0b0962687b8b4eb84c8bc840d
Matt Thomas <matsinet@gmail.com>
2023-06-09 13:35:43 -0500

    Add error handling with notification component

---
+++ components/notification.js
@@ -0,0 +1,10 @@
+import html from "html-literal";
+
+export default (state) => {
+  return html`
+    <dialog id="notification" ${state.visible ? 'open' : ''} ${state.type ? `class="dialog-${state.type}"` : ''}>
+      ${state.message}
+      ${state.dismissable ? html`<form method="dialog"><button>OK</button></form>` : ""}
+    </dialog>
+  `;
+}
--- store/order.js
+++ store/order.js
@@ -1,5 +1,5 @@
 export default {
   header: "Order a pizza!",
   view: "order",
-  availableToppings: ["Chicken", "Onion", "Spinach", "Extra Cheese", "Red Pepper", "Pepperoni"],
+  availableToppings: ["Chicken", "Onion", "Spinach", "Extra Cheese", "Red Pepper", "Pepperoni", "Ham", "Bacon"],
 };
