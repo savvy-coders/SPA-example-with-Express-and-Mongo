41b7f9b1e10265de6bc847b9bb862adad265f518
Matt Thomas <matsinet@gmail.com>
2023-08-15 13:21:44 -0500

    Add image aggregator example

---
+++ assets/img/index.js
@@ -0,0 +1,2 @@
+export dogWithFlower from "./dog-with-flower.jpg";
+export oops404 from "./oops-404.jpg";
--- views/viewNotFound.js
+++ views/viewNotFound.js
@@ -1,7 +1,12 @@
 import html from "html-literal";
+import "../assets/css/views/viewNotFound.css";
+import * as images from "../assets/img/";
 
 export default (state) => html`
-  <h3>
-    You be lost, go somewhere else!
-  </h3>
+  <div id="oops404">
+    <img src="${images.oops404}" alt="View not found!">
+    <div class="attribution">
+      <a href="https://www.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_13315300.htm" target="_blank">Image by storyset</a> on Freepik
+    </div>
+  </div>
 `;
